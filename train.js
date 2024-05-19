a9d09851c07061e6a105b8781566cac76fae1dee
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-05-20 01:47:42 +0900

    Final version of publishing project with Task A solution in train.js file

--- train.js
+++ train.js
@@ -1,1 +1,11 @@
-console.log("Train task ishga tushdi!");
+function countLetter(letter, word){
+    let count = 0;
+    for(let i=0; i<word.length; i++){
+      if (word[i] === letter){
+        count+=1;
+      }
+    }
+    return count;
+  }
+  
+  console.log(countLetter('e', 'engineer'));
