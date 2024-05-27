304a0b8f14126d3209f60b24249560814528f122
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-05-28 01:07:55 +0900

    feat: D TASK solution in train

--- train.js
+++ train.js
@@ -8,6 +8,9 @@
 //     }
 //     return count;
 //   }
+
+const e = require("express");
+
   
 //   console.log(countLetter('e', 'engineer'));  
 
@@ -110,49 +113,75 @@
 // console.log(countDigits("ad2a54y79wet0sfgb9"));
 
 
-//TASK C
-let time = new Date();
+// TASK C
+// let time = new Date();
 
-class Shop{
-    non;
-    lagmon;
-    cola;
-    constructor(non, lagmon, cola){
-        this.non = non;
-        this.lagmon = lagmon;
-        this.cola = cola;
-    }
+// class Shop{
+//     non;
+//     lagmon;
+//     cola;
+//     constructor(newNon, newLagmon, newCola){
+//         this.non = newNon;
+//         this.lagmon = newLagmon;
+//         this.cola = newCola;
+//     }
 
-    qoldiq(){
-        console.log("hozir " + time.getHours() +":"+time.getMinutes()+":"+time.getSeconds());
-        console.log(this.non+"ta non,"+this.lagmon+"ta lagmon va "+this.cola+"ta cola mavjud!");
-    }
-    sotish(product, quantity){
-        if(product == 'non')
-            this.non -= quantity;
-        else if(product == 'lagmon')
-            this.lagmon -= quantity;
-        else if(product == 'cola')
-            this.cola -= quantity;
-        else
-            console.log("this product is not availabe");
-    }
-    qabul(product, quantity){
-        if(product == 'non')
-            this.non += quantity;
-        else if(product == 'lagmon')
-            this.lagmon += quantity;
-        else if(product == 'cola')
-            this.cola += quantity;
-        else
-            console.log("this type of product is not availabe");
-    }
+//     qoldiq(){
+//         console.log(`hozir ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`);
+//         //console.log("hozir " + time.getHours() +":"+time.getMinutes()+":"+time.getSeconds());
+//         console.log(this.non+"ta non,"+this.lagmon+"ta lagmon va "+this.cola+"ta cola mavjud!");
+//         // console.log(`${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cole mavjud!`);
+//     }
+//     sotish(product, quantity){
+//         if(product == 'non')
+//             this.non -= quantity;
+//         else if(product == 'lagmon')
+//             this.lagmon -= quantity;
+//         else if(product == 'cola')
+//             this.cola -= quantity;
+//         else
+//             console.log("this product is not availabe");
+//     }
+//     qabul(product, quantity){
+//         if(product == 'non')
+//             this.non += quantity;
+//         else if(product == 'lagmon')
+//             this.lagmon += quantity;
+//         else if(product == 'cola')
+//             this.cola += quantity;
+//         else
+//             console.log("this type of product is not availabe");
+//     }
 
+// }
+
+// const shop = new Shop(4, 5, 2);
+// shop.qoldiq();
+// shop.sotish('non', 3);
+// shop.qoldiq();
+// shop.qabul('cola', 4);
+// shop.qoldiq();
+
+//TASK-D
+
+function checkContent(firstWord, secondWord){
+    let flag = false; 
+    if (firstWord.length == secondWord.length){
+        flag = true;
+        for(let i = 0; i<firstWord.length; i++){
+            if(secondWord.includes(firstWord[i])){
+                secondWord.replace(firstWord[i],"")
+            }
+            else{
+                flag = false;
+                break;
+            }
+        }
+    }
+    return flag;
 }
 
-const shop = new Shop(4, 5, 2);
-shop.qoldiq();
-shop.sotish('non', 3);
-shop.qabul('cola', 4);
-shop.qoldiq();
+console.log(checkContent("ffalse", "eslfaf"));
+
+
 
